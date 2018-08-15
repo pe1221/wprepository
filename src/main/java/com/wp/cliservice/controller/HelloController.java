6013d32dc7384c71d779dd0fd4734966de329932
pe1221 <pe1221@qq.com>
2018-08-15 14:30:37 +0800

    啊啊

---
+++ src/main/java/com/wp/cliservice/controller/HelloController.java
@@ -0,0 +1,43 @@
+/*package com.wp.cliservice.controller;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+
+@EnableDiscoveryClient
+@RestController
+@SpringBootApplication
+public class HelloController {
+	private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
+	@Autowired
+	private DiscoveryClient discoverClient;
+	
+	@RequestMapping(value="getHello",method=RequestMethod.GET)
+	public String getHello() {
+		logger.info("DiscoveryClient"+discoverClient.toString());
+		List<String>  services =discoverClient.getServices();
+		for (String service : services) {
+			logger.info("--------------serviceUrl----------:"+service);
+		}
+		
+		return "hello world";
+	}
+
+	
+	public static void main(String[] args) {
+		SpringApplication.run(HelloController.class, args);
+	}
+	
+}
+*/
